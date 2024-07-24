75c4b3b9623276109a84d344544a49e2e11f25b6
Michael Ruf <git@michiruf.de>
2024-07-24 19:13:03 +0200

    Make update hosts more failsafe and more verbose

--- update-hosts-file.sh
+++ update-hosts-file.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+hosts_file=/mnt/c/Windows/System32/drivers/etc/hosts
+hosts_template_file=/mnt/c/Windows/System32/drivers/etc/hosts_template
+
+if [ ! -f "$hosts_template_file" ]; then
+  echo "Cannot update hosts file, since there is no template file ($hosts_template_file)"
+  exit 1
+fi
+
 script_dir=$(dirname "$0")
 updated_hosts="$(ls "$script_dir/sites" | sed "s/^/127.0.0.1 /" | sed "s/$/.local/" | tr '\n' '@' | sed 's/@/\\n/g')"
 sed "
@@ -9,4 +17,4 @@ sed "
   }
   /# update-hosts-file start/a\\
 $updated_hosts
-}" /mnt/c/Windows/System32/drivers/etc/hosts_template > /mnt/c/Windows/System32/drivers/etc/hosts
+}" $hosts_template_file > $hosts_file

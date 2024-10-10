826cc03a056d11863a932e7b62fe7a848a10a7f5
Michael Ruf <git@michiruf.de>
2024-10-10 12:24:21 +0200

    When running sail php as the correct user, an error occurred

--- sail-php.sh
+++ sail-php.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# If we are already the sail user, just execute php
+if [ "$(whoami)" = "sail" ]; then
+  exec php "$@"
+fi
+
+# In case gosu is not available (which it is in sail), install it
 if ! command -v gosu &> /dev/null; then
 	apt-get update
 	apt-get install -y gosu

c825925bed5e4a8009ec4555b7e2c7a7b72fe978
Michael Ruf <git@michiruf.de>
2024-10-14 13:47:30 +0200

    Add mailpit example

--- update-hosts-file.sh
+++ update-hosts-file.sh
@@ -10,11 +10,12 @@ fi
 
 script_dir=$(dirname "$0")
 updated_hosts="$(ls "$script_dir/sites" | sed "s/^/127.0.0.1 /" | sed "s/$/.local/" | tr '\n' '@' | sed 's/@/\\n/g')"
+updated_hosts_mailpit="$(ls "$script_dir/sites" | sed "s/^/127.0.0.1 mail./" | sed "s/$/.local/" | tr '\n' '@' | sed 's/@/\\n/g')"
 sed "
 /# update-hosts-file start/,/# update-hosts-file end/{
   /# update-hosts-file start/!{
     /# update-hosts-file end/!d
   }
   /# update-hosts-file start/a\\
-$updated_hosts
+$updated_hosts$updated_hosts_mailpit
 }" $hosts_template_file > $hosts_file

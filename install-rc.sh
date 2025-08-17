d61df4a834a2dbf2640d9a8b88ddcb2f56e1c7e3
Michael Ruf <git@michiruf.de>
2025-08-17 14:50:20 +0200

    Refactor the install shell file

--- install-rc.sh
+++ install-rc.sh
@@ -2,9 +2,14 @@
 
 SCRIPT_DIRECTORY=$(dirname "$(realpath "$0")")
 
-echo '' >> ~/.bashrc
-echo '' >> ~/.bashrc
-echo '# Laravel Runtime' >> ~/.bashrc
+echo >> ~/.bashrc
+echo "# Laravel Runtime" >> ~/.bashrc
 echo "export LARAVEL_RUNTIME_DIRECTORY=$SCRIPT_DIRECTORY" >> ~/.bashrc
-echo 'source $LARAVEL_RUNTIME_DIRECTORY/.bashrc' >> ~/.bashrc
+cat >>~/.bashrc <<'EOF'
+if [[ -d "$LARAVEL_RUNTIME_DIRECTORY" ]]; then
+  source $LARAVEL_RUNTIME_DIRECTORY/.bashrc
+else
+  echo "Laravel runtime not found in '$LARAVEL_RUNTIME_DIRECTORY'" >&2
+fi
+EOF
 echo 'Validate your uprated ~/.bashrc file, then source it'

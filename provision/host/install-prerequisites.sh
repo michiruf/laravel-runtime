dfd43ff2007285c919cf49e857fab252600e1a3f
Michael Ruf <git@michiruf.de>
2025-09-24 14:11:21 +0200

    Install composer properly using their installation method

--- provision/host/install-prerequisites.sh
+++ provision/host/install-prerequisites.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Remove previous versions
 sudo apt remove php8.3-*
@@ -9,6 +9,10 @@ sudo apt update
 sudo apt install php8.4 php8.4-cli php8.4-fpm php8.4-{bz2,curl,mbstring,intl,zip,dom,bcmath}
 
 # Install composer
-sudo apt install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'ed0feb545ba87161262f2d45a633e34f591ebb3381f2e0063c345ebea4d228dd0043083717770234ec00c5a9f9593792') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
 
 # TODO Perform further the steps to prepare the wsl machine when they come up

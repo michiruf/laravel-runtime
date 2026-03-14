117aa77936a14aafddaa17517faa898f37e4c964
Michael Ruf <git@michiruf.de>
2026-03-14 16:33:32 +0100

    Integrate the host template into the env

--- services/update-wsl-hosts-file/service.sh
+++ services/update-wsl-hosts-file/service.sh
@@ -4,25 +4,24 @@
 # Requires $LARAVEL_RUNTIME_DIRECTORY to be set
 if [ -z ${LARAVEL_RUNTIME_DIRECTORY+x} ]; then
     echo 'LARAVEL_RUNTIME_DIRECTORY environment variable must be set'
-    return 1
+    exit 1
 fi
 
 hosts_file=/mnt/c/Windows/System32/drivers/etc/hosts
-hosts_template_file=/mnt/c/Windows/System32/drivers/etc/hosts_template
 
-if [ ! -f "$hosts_template_file" ]; then
-  echo "Cannot update hosts file, since there is no template file ($hosts_template_file)"
-  exit 1
+if [ -z "${WSL_HOSTS_TEMPLATE}" ]; then
+    echo "WSL_HOSTS_TEMPLATE must be set in .env"
+    exit 1
 fi
 
 sites="$(find "$LARAVEL_RUNTIME_DIRECTORY/sites" -name docker-compose.yml -printf '%h\n' | xargs -I{} basename {})"
 updated_hosts="$(echo "$sites" | sed "s/^/127.0.0.1 /" | sed "s/$/.local/" | tr '\n' '@' | sed 's/@/\\n/g')"
 updated_hosts_mailpit="$(echo "$sites" | sed "s/^/127.0.0.1 mail./" | sed "s/$/.local/" | tr '\n' '@' | sed 's/@/\\n/g')"
-sed "
+echo "$WSL_HOSTS_TEMPLATE" | sed "
 /# update-hosts-file start/,/# update-hosts-file end/{
   /# update-hosts-file start/!{
     /# update-hosts-file end/!d
   }
   /# update-hosts-file start/a\\
 $updated_hosts$updated_hosts_mailpit
-}" $hosts_template_file > $hosts_file
+}" > $hosts_file
